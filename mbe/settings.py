41a1483d2b532b28098dc4bf1fc8e2153c583912
jazdelu <lushizhao@qq.com>
2015-10-21 01:49:32 +0800

    .

--- mbe/settings.py
+++ mbe/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'mbe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mbe',
+        'USER': 'jazdelu',
+        'PASSWORD': 'lushizhao1129',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
